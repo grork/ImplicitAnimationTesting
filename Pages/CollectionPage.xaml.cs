5377a8b7ea45b7a2bcfc3cad52a2e65deaa16e11
Dominic Hopton <d.hopton@codevoid.net>
2017-08-05 15:50:55 -0700

    Adding Collection Implicit Animations

--- Pages/CollectionPage.xaml.cs
+++ Pages/CollectionPage.xaml.cs
@@ -6,10 +6,12 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -28,14 +30,62 @@ namespace ImplicitAnimations.Pages
     /// </summary>
     public sealed partial class CollectionPage : Page
     {
+        static bool s_firstNav = true;
+
         public CollectionPage()
         {
             this.InitializeComponent();
+
+            Canvas.SetZIndex(this, 10);
 
             this.GenerateData().ContinueWith((result) =>
             {
                 this.CollectionList.ItemsSource = result.Result;
             }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            var animationDuration = TimeSpan.FromSeconds(1.0f);
+
+            var compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            if(!s_firstNav)
+            {
+                var collectionIntroAnimation = compositor.CreateScalarKeyFrameAnimation();
+                collectionIntroAnimation.Duration = animationDuration;
+                collectionIntroAnimation.SetReferenceParameter("A", ElementCompositionPreview.GetElementVisual(this.CollectionList));
+                collectionIntroAnimation.InsertExpressionKeyFrame(0.0f, "A.Size.y + this.StartingValue");
+                collectionIntroAnimation.InsertExpressionKeyFrame(1.0f, "this.StartingValue");
+                collectionIntroAnimation.Target = "Offset.Y";
+
+                ElementCompositionPreview.SetImplicitShowAnimation(this.CollectionList, collectionIntroAnimation);
+
+                var headerIntroAnimation = compositor.CreateScalarKeyFrameAnimation();
+                headerIntroAnimation.Duration = animationDuration;
+                headerIntroAnimation.SetReferenceParameter("A", ElementCompositionPreview.GetElementVisual(this.CollectionList));
+                headerIntroAnimation.InsertExpressionKeyFrame(0.0f, "A.Size.y + A.Offset.Y + this.StartingValue");
+                headerIntroAnimation.InsertExpressionKeyFrame(0.01f, "A.Size.y + A.Offset.Y + this.StartingValue");
+                headerIntroAnimation.InsertExpressionKeyFrame(1.0f, "this.StartingValue");
+                headerIntroAnimation.Target = "Offset.Y";
+
+                ElementCompositionPreview.SetImplicitShowAnimation(this.Header, headerIntroAnimation);
+            }
+
+            var collectionExitAnimation = compositor.CreateScalarKeyFrameAnimation();
+            collectionExitAnimation.Duration = animationDuration;
+            collectionExitAnimation.SetReferenceParameter("A", ElementCompositionPreview.GetElementVisual(this.CollectionList));
+            collectionExitAnimation.InsertExpressionKeyFrame(0.0f, "this.StartingValue");
+            collectionExitAnimation.InsertExpressionKeyFrame(1.0f, "-(A.Size.y)");
+            collectionExitAnimation.Target = "Offset.Y";
+
+            ElementCompositionPreview.SetImplicitHideAnimation(this.CollectionList, collectionExitAnimation);
+
+            var headerExitAnimation = compositor.CreateScalarKeyFrameAnimation();
+            headerExitAnimation.Duration = animationDuration;
+            headerExitAnimation.SetReferenceParameter("A", ElementCompositionPreview.GetElementVisual(this.Header));
+            headerExitAnimation.InsertExpressionKeyFrame(0.0f, "this.StartingValue");
+            headerExitAnimation.InsertExpressionKeyFrame(1.0f, "-(A.Size.y)");
+            headerExitAnimation.Target = "Offset.Y";
+            ElementCompositionPreview.SetImplicitHideAnimation(this.Header, headerExitAnimation);
+
+            s_firstNav = false;
         }
 
         public async Task<IList<CollectionItem>> GenerateData()
