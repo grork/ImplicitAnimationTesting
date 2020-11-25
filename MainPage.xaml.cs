490f32eee8f2d206c67461934358b1776146050d
Dominic Hopton <d.hopton@codevoid.net>
2020-11-25 15:35:02 -0800

    Update to VS2019 + OS Version of WinUI
    - Updates the project, solution to be VS2019
    - Update SDK to latest version of the SDK
    - Fix back button being displayed when it wasn't needed

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using ImplicitAnimations.Pages;
-using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,7 +10,6 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
